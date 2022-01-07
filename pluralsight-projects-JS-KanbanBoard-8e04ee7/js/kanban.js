4f7493e073ff18ac72bbb0a03c8ba65c0fc72feb
Kay0s <hamilton.kristina@gmail.com>
2022-01-07 16:49:52 -0600

    added event listener to save button

--- pluralsight-projects-JS-KanbanBoard-8e04ee7/js/kanban.js
+++ pluralsight-projects-JS-KanbanBoard-8e04ee7/js/kanban.js
@@ -31,6 +31,11 @@ const create_item = () => {
 
   let save_btn = document.createElement("button");
   save_btn.innerHTML = "Save";
+  save_btn.addEventListener("click", () => {
+    
+  }
+
+  )
 
 };
 
